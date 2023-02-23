f17b18753a0f929f2d2b5639ec4c83e96aee3ae5
esgalli <everton.sgalli@gmail.com>
2023-02-23 16:53:00 -0300

    Primeiro Commit

---
+++ RefatorandoCodigo/Inwords/DigitoUnidade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefatorandoCodigo.Inwords
+{
+    class DigitoUnidade : Digito
+    {
+        public DigitoUnidade(long numero, double posicao, Digito digitoFilho) : base(numero, posicao, digitoFilho) { }
+    }
+}
---
+++ RefatorandoCodigo/Inwords/ResourceManagerHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Resources;
+using System.Text;
+
+namespace RefatorandoCodigo.Inwords
+{
+    class ResourceManagerHelper
+    {
+        private readonly ResourceManager resourceManager;
+        private static ResourceManagerHelper instance;
+        private ResourceManagerHelper()
+        {
+            resourceManager = new ResourceManager(@"RefatorandoCodigo.Properties.messages_pt_BR",
+             System.Reflection.Assembly.Load(new System.Reflection.AssemblyName("RefatorandoCodigo")));
+        }
+
+        public static ResourceManagerHelper Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ResourceManagerHelper();
+                return instance;
+            }
+        }
+
+        public ResourceManager ResourceManager
+        {
+            get
+            {
+                return resourceManager;
+            }
+        }
+    }
+}
